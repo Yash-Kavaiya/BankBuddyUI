fb162d003922fbacd07bb1da0f5cc9a5cf465c6b
Yash Kavaiya <yash.kavaiya3@gmail.com>
2024-05-08 15:16:22 +0000

    Complted dialogflow_api work

--- dialogflow_api.py
+++ dialogflow_api.py
@@ -1,1 +1,57 @@
-from google.cloud import dialogflowcx_v3beta1 as dialogflow
+import uuid
+# https://cloud.google.com/dialogflow/cx/docs/quick/api
+from google.cloud.dialogflowcx_v3beta1.services.agents import AgentsClient
+from google.cloud.dialogflowcx_v3beta1.services.sessions import SessionsClient
+from google.cloud.dialogflowcx_v3beta1.types import session
+
+
+def detect_intent_texts(agent, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    session_path = f"{agent}/sessions/{session_id}"
+    print(f"Session path: {session_path}\n")
+    client_options = None
+    agent_components = AgentsClient.parse_agent_path(agent)
+    location_id = agent_components["location"]
+    if location_id != "global":
+        api_endpoint = f"{location_id}-dialogflow.googleapis.com:443"
+        print(f"API Endpoint: {api_endpoint}\n")
+        client_options = {"api_endpoint": api_endpoint}
+    session_client = SessionsClient(client_options=client_options)
+
+    for text in texts:
+        text_input = session.TextInput(text=text)
+        query_input = session.QueryInput(text=text_input, language_code=language_code)
+        request = session.DetectIntentRequest(
+            session=session_path, query_input=query_input
+        )
+        response = session_client.detect_intent(request=request)
+
+        print("=" * 20)
+        print(f"Query text: {response.query_result.text}")
+        response_messages = [
+            " ".join(msg.text.text) for msg in response.query_result.response_messages
+        ]
+        print(f"Response text: {' '.join(response_messages)}\n")
+    res ={
+        'texts':texts,
+        'res_msg':response_messages
+          }
+    return res
+
+def run_sample(texts,session_id):
+    # TODO(developer): Replace these values when running the function
+    project_id = "durable-pulsar-419609"
+    # For more information about regionalization see https://cloud.google.com/dialogflow/cx/docs/how/region
+    location_id = "global"
+    # For more info on agents see https://cloud.google.com/dialogflow/cx/docs/concept/agent
+    agent_id = "25adf82d-4712-4926-a32a-bb743f695294"
+    agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
+    # For more information on sessions see https://cloud.google.com/dialogflow/cx/docs/concept/session
+    session_id = uuid.uuid4()
+    texts = ["Hello"]
+    # For more supported languages see https://cloud.google.com/dialogflow/es/docs/reference/language
+    language_code = "en-us"
+    detect_intent_texts(agent, session_id, texts, language_code)
+    return res
